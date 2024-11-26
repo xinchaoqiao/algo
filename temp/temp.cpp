507abdadf154986b7d667fdc7de1720ff4daf779
北之信念 <xinchaoqiao6@163.com>
2024-11-26 18:38:54 +0800

    洛谷temp

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
+int n;
 int main() {
+    scanf("%d", &n);
+    bool flag = false;
+    int sum = 0, putin = 0;
+    int i = 0;
+    while (sum != n) {
+        scanf("%d", &putin);
+        if (!flag) {
+            for (i ;i < putin;i++) {
+                printf("%d", 0);
+                if (i % n == 0) puts("");
+            }
+        }
+        else {
+            for (i;i < putin;i++) {
+                printf("%d", 1);
+                if (i % n == 0) puts("");
+            }
+        }
+        sum += putin;
+    }
+
     return 0;
 }
