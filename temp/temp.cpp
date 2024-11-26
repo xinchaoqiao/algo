915f193dd8a4e74ff13f8bb0a8c75bf432eeb2fe
北之信念 <xinchaoqiao6@163.com>
2024-11-27 00:55:24 +0800

    洛谷压缩技术续集

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -5,22 +5,25 @@ int main() {
     scanf("%d", &n);
     bool flag = false;
     int sum = 0, putin = 0;
-    int i = 0;
-    while (sum != n) {
+    int pn = 1;
+    while (true) {
         scanf("%d", &putin);
+        sum += putin;
         if (!flag) {
-            for (i ;i < putin;i++) {
+            for (int i = 0;i < putin;i++,pn++) {
                 printf("%d", 0);
-                if (i % n == 0) puts("");
+                if (pn % n == 0) printf("\n");
             }
+            flag = !flag;
         }
         else {
-            for (i;i < putin;i++) {
+            for (int i = 0;i < putin;i++,pn++) {
                 printf("%d", 1);
-                if (i % n == 0) puts("");
+                if (pn % n == 0) printf("\n");
             }
+            flag = !flag;
         }
-        sum += putin;
+        if (sum == n * n) break;
     }
 
     return 0;
--- temp/temp2.cpp
+++ temp/temp2.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "hello world" << endl;
+	string str;
+	cin >> str;
+	int count = 0;
+	bool flag = false;
+	for (int i = 0;i < sizeof str;i++) {
+		if (str[i] == '0' && flag == false) count++;
+		else {
+			cout << count << " ";
+			count = 0;
+			flag = !flag;
+		}
+
+		if (str[i] == '1' && flag == true) count++;
+		else {
+			cout << count << " ";
+			count = 0;
+			flag = !flag;
+		}
+
+	}
 	return 0;
 }
