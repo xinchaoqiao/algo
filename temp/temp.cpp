60284d865deb652ecc859b773d4a98cbcb42f413
北之信念 <xinchaoqiao6@163.com>
2024-11-26 16:44:18 +0800

    temp改变

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,33 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-
-void MaxLength(char *str) {
-    int start = 0, end = 0, maxLen = 0, len = 0;
-    int n = strlen(str);
-
-    for (int i = 0; i < n; i++) {
-        if (str[i] != ' ') {
-            len++;
-            if (len > maxLen) {
-                maxLen = len;
-                start = end;
-            }
-        } else {
-            len = 0;
-            end = i + 1;
-        }
-    }
-
-    for (int i = start; i < start + maxLen; i++) {
-        printf("%c", str[i]);
-    }
-    printf("\n");
-}
-
+#include <iostream>
+using namespace std;
 int main() {
-    char str[100];
-    //fgets(str, sizeof(str), stdin); // 读取字符串
-    gets(str);
-    MaxLength(str);
     return 0;
 }
