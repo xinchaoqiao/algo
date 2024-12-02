d0771defd56d95cb3dad428a6fcadf25e645cc0b
北之信念 <xinchaoqiao6@163.com>
2024-12-03 00:02:10 +0800

    up

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
-const int N = 110;
-const int M = 110;
+
 int n, m;
 
 int main(void) {
     cin >> n >> m;
-    
+    int s1 = 0, s2 = 0;
+    s2 = ((m + 1) * (n + 1) * n * m) / 4;
+    for (;m >= 0 && n >= 0;m--, n--) {
+        s1 += m * n;
+    }
+    cout << s1 << " " << s2-s1 << endl;
     return 0;
 }
