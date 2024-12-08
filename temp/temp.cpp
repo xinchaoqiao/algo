4325b20291b26aae0d1189f9de7a2afd54a7c23b
北之信念 <xinchaoqiao6@163.com>
2024-12-09 00:05:47 +0800

    tab

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int n, m;
 int arr[N];
 int main() {
-  cin >> n >> m;
-  for (int i = 0;i < n;i++) {
-    cin >> arr[i];
-  }
-  int max = arr[1] + arr[2] + arr[0], count = 0;
-  for (int i = m - 1;i < n;i++) {
-    count = 0;
-    int tm = m - 1;
-    while (tm >= 0) {
-      count += arr[tm--];
+    cin >> n >> m;
+    for (int i = 0;i < n;i++) {
+        cin >> arr[i];
     }
-    if (max > count) max = count;
+    int max = arr[1] + arr[2] + arr[0], count = 0;
+    for (int i = m - 1;i < n;i++) {
+        count = 0;
+        int tm = m - 1;
+        while (tm >= 0) {
+            count += arr[tm--];
+        }
+        if (max > count) max = count;
 
-  }
-  cout << max << endl;
-  return 0;
+    }
+    cout << max << endl;
+    return 0;
 }
--- temp/temp2.cpp
+++ temp/temp2.cpp
@@ -3,11 +3,11 @@
 #include <algorithm>
 using namespace std;
 int main(void) {
-  vector<int> v;
-  v.push_back(1);
-  v.push_back(0);
-  v.push_back(3);
-  int rel = *max_element(v.begin(), v.end());
-  cout << rel << endl;
-  return 0;
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(3);
+    int rel = *max_element(v.begin(), v.end());
+    cout << rel << endl;
+    return 0;
 }
