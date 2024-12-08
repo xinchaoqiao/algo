cf82d41baa41573cac72ba49a1b1513adec5f0f9
北之信念 <xinchaoqiao6@163.com>
2024-12-08 22:12:32 +0800

    洛谷

---
+++ temp/temp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+const int N = 3e3 + 10;
+using namespace std;
+int n, m;
+int arr[N];
+int main() {
+  cin >> n >> m;
+  for (int i = 0;i < n;i++) {
+    cin >> arr[i];
+  }
+  int max = arr[1] + arr[2] + arr[0], count = 0;
+  for (int i = m - 1;i < n;i++) {
+    count = 0;
+    int tm = m - 1;
+    while (tm >= 0) {
+      count += arr[tm--];
+    }
+    if (max > count) max = count;
+
+  }
+  cout << max << endl;
+  return 0;
+}
