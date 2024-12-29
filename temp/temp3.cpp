a644dff7acfd69597fb33b432926ecc200e0d488
北之信念 <xinchaoqiao6@163.com>
2024-12-29 16:02:46 +0800

    12/29

--- temp/temp3.cpp
+++ temp/temp3.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    queue<string> q;
-    string start = "1213";
-    string end = "121";
-    q.push(start);
-    q.push(end);
-    cout << q.size() << endl;
-    while (q.size()) {
-        cout << q.front() << endl;
-        q.pop();
-    }
+    cout << "hello world" << endl;
     return 0;
 }
