4bee48d0ec0ad901cd8ddf31e4e9b33e213f252e
北之信念 <xinchaoqiao6@163.com>
2024-12-31 00:22:58 +0800

    substr纠错

--- string截取substr函数.md
+++ string截取substr函数.md
@@ -2,11 +2,11 @@
 
 `substr(len)`从pos位置截取到字符串结尾
 
-`substr(offset,len)` 从0 截取到 pos 位置 【0，pos）前包后不包
+`substr(offset,len)` 从0 截取到 pos 位置 【0，pos】
 
 如果截取某一段的字符串
 
-`substr(i,pos-i)`eg: 111111.11 i = 2 pos = 5，2,3则 答案：1111.1
+`substr(i,pos-i)`eg: 111111.11 i = 2 pos = 5 则 答案：1111.
 
 string find（） 函数 如果没找到  将会=string::npos
 
--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
 
-int solution(const int n, const int H, const int A, vector<int> h, vector<int> a) {
+int solution(int a, int b) {
     // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
     // write code here
-    struct Monster {
-        int health;
-        int attack;
-
-
-        bool operator<(const Monster& other)const {
-            return health < other.health && attack < other.attack;
-        }
-    };
-    // 存储每个怪物信息
-    vector<Monster> monster(n);
-    for (int i = 0;i < n;i++) {
-        monster[i] = { h[i],a[i] };
-    }
-
-    sort(monster.begin(), monster.end(), [](const Monster& m1, const Monster& m2) {
-        if (m1.health == m2.health) return m1.attack < m2.attack;
-        return m1.health < m2.health;
-        });
-    for (int i = 0;i < n;i++) {
-        cout << "i = " << i << ' ' << monster[i].health << ' ' << monster[i].attack << endl;
-    }
-    int rel = 0;
-    for (int i = 0;i < n;i++) {
-        if (monster[i].health >= H || monster[i].attack >= A) continue;
-        if (monster[i].health < H && monster[i].attack < A) {
-            rel++;
+    string sb = to_string(b);
+    string sa = to_string(a);
+    string s_max = sa + sb;
+    for (int i = 0;i <= sa.length();i++) {
+        string cur = sa.substr(0, i) + sb + sa.substr(i);
+        if (s_max < cur) {
+            s_max = cur;
         }
     }
-    return rel;
+    cout << s_max << endl;
+    return stoi(s_max);
 }
 
 int main() {
-    cout << (solution(3, 4, 5, { 1, 2, 3 }, { 3, 2, 1 }) == 1) << endl;
-    cout << (solution(5, 10, 10, { 6, 9, 12, 4, 7 }, { 8, 9, 10, 2, 5 }) == 2) << endl;
-    cout << (solution(4, 20, 25, { 10, 15, 18, 22 }, { 12, 18, 20, 26 }) == 3) << endl;
-    cout << (solution(13, 17, 14, { 2,3,12,13,7,8,5,15,1,12,17,14,4 }, { 13,13,8,15,2,5,7,8,13,7,6,8,1 }) == 4) << endl;
+    cout << (solution(76543, 4) == 765443) << endl;
+    cout << (solution(1, 0) == 10) << endl;
+    cout << (solution(44, 5) == 544) << endl;
+    cout << (solution(666, 6) == 6666) << endl;
+    cout << (solution(17, 1) == 171) << endl;
     return 0;
 }
