b1d2dd0cb79a3651bda0d34564e5716ef465bbb2
北之信念 <xinchaoqiao6@163.com>
2024-12-30 10:40:49 +0800

    12/30

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,16 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
+int solution(int n) {
+  // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
+  // write code here
+  return n-1;
+}
+
 int main() {
-    cout << " hello worle" << endl;
-    return 0;
+  cout << (solution(7) == 6) << endl;
+  cout << (solution(14) == 13) << endl;
+  cout << (solution(1) == 0) << endl;
+
+  return 0;
 }
