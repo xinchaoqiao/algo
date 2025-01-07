87287f0e72aa382de00ae235b4383f24fac53357
北之信念 <xinchaoqiao6@163.com>
2025-01-07 23:09:13 +0800

    1/7

--- temp/temp3.cpp
+++ temp/temp3.cpp
@@ -1,6 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main() {
-    cout << "hello world" << endl;
+int main(void) {
+    vector<int> nums = { 15,12,15 };
+
+    sort(nums.begin(), nums.end(), [](int a, int b) { return a > b; });
+    auto it = unique(nums.begin(), nums.end());
+    nums.erase(it, nums.end());
+    for (int i = 0;i < nums.size();i++) {
+        cout << nums[i] << endl;
+    }
     return 0;
 }
