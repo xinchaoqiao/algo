8912b24828c71f391f3fbb123566747cdb3e01bd
北之信念 <xinchaoqiao6@163.com>
2025-01-07 22:04:00 +0800

    test

--- temp/temp1.cpp
+++ temp/temp1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(void) {
     string a = "111111.11";
     string b = "2222";
-    cout << (a.substr(2,5)) << endl;
+    cout << (a.substr(2, 5)) << endl;
+    cout << "hello world" << endl;
     return 0;
 }
