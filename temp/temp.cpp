65ae74369829136429b1e066460b3176072c4735
北之信念 <xinchaoqiao6@163.com>
2025-01-21 15:03:36 +0800

    Add grade_demo_map

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
-#include <vector>
+#include <set>
+#include <map>
 using namespace std;
-int solution(int m, int n, std::vector<std::vector<int>> p) {
-  // Edit your code here
-  int sum = 0;
-  int cmax = m > n ? m : n;
-  vector<int> v(cmax+1, 0);
-
-  for (auto fir : p) {
-    v[fir[0]] = fir[1];
-  }
-  for (int i = 1; i < m; i++) {
-    if (v[i] > v[i - 1] || v[i] == 0) {
-      v[i] = v[i - 1];
-    }
-  }
-  for (int i = 0; i < m; i++) {
-    sum += v[i];
-  }
-  // cout << sum << endl;
-  return sum;
-}
-
 int main() {
-  // Add your test cases here
-  std::cout << (solution(5, 4, {{0, 2}, {1, 3}, {2, 1}, {3, 2}}) == 7);
-  return 0;
+    map<string, map<string, int>> my_map = {
+        {"王力宏",{{"语文",88},{"数学",99}}},
+        {"周杰伦",{{"语文",77},{"数学",22}} }
+    };
+    for (auto mm : my_map) {
+        cout << "姓名：" << mm.first << endl;
+        for (auto m : mm.second) {
+            cout << m.first <<"： " << m.second << endl;
+        }
+    }
+    return 0;
 }
