91e81ca309898441399d72722b564ed71e873d78
北之信念 <xinchaoqiao6@163.com>
2025-01-21 11:49:18 +0800

    1_21刷题

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,20 +1,29 @@
-#include <cctype>
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 using namespace std;
+int solution(int m, int n, std::vector<std::vector<int>> p) {
+  // Edit your code here
+  int sum = 0;
+  int cmax = m > n ? m : n;
+  vector<int> v(cmax+1, 0);
 
-int solution(const string& s) {
-    // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
-    // write code here
-    string tm(s.size(), ' ');
-    transform(s.begin(),s.end(),tm.begin(),::tolower);
-    cout << tm << endl;
-    return 0;
+  for (auto fir : p) {
+    v[fir[0]] = fir[1];
+  }
+  for (int i = 1; i < m; i++) {
+    if (v[i] > v[i - 1] || v[i] == 0) {
+      v[i] = v[i - 1];
+    }
+  }
+  for (int i = 0; i < m; i++) {
+    sum += v[i];
+  }
+  // cout << sum << endl;
+  return sum;
 }
 
 int main() {
-    cout << (solution("AUBTMKAxfuu") == 1) << endl;
-    return 0;
+  // Add your test cases here
+  std::cout << (solution(5, 4, {{0, 2}, {1, 3}, {2, 1}, {3, 2}}) == 7);
+  return 0;
 }
--- temp/temp1.cpp
+++ temp/temp1.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(void) {
-    for (int i = 1; i <= 9;i++) {
-        for (int j = 1; j <= i ;j++) {
-            cout << j << "*" << i << "=" << j * i << "\t" << " ";
-        }
-        cout << endl;
+int solution(int m, int n, std::vector<std::vector<int>> p) {
+    // Edit your code here
+    int sum = 0;
+    vector<int> v(m, 0);
+    for (int i = 0;i < m;i++) {
+        
+        cout << p[i][0] << endl;
     }
+    return sum;
+}
+
+int main() {
+    // Add your test cases here
+    std::cout << (solution(5, 4, {{0, 2}, {1, 3}, {2, 1}, {3, 2}}) == 7);
     return 0;
 }
