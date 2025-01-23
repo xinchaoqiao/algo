3ed96229b87eae0bce11cb6ad2c28c2059a3efd2
北之信念 <xinchaoqiao6@163.com>
2025-01-24 00:59:11 +0800

    添加滑动窗口注释

--- algo/Slide Window/food_hike_problem.cpp
+++ algo/Slide Window/food_hike_problem.cpp
@@ -1,10 +1,14 @@
+/*
+    滑动窗口 —— 单调队列
+
+*/
 #include <iostream>
 #include <vector>
 #include <deque>
 using namespace std;
 int solution(int n, int k, std::vector<int> data) {
     // Edit your code here
-    deque<pair<int,int>> mins;
+    deque<pair<int, int>> mins;// 双端队列 插入 index 和 value
     int rel = 0;
     for(int i = 0;i < n;i++){
         // 队列不为空 且 队尾元素大于当前元素时 移除队尾元素
@@ -12,7 +16,7 @@ int solution(int n, int k, std::vector<int> data) {
             mins.pop_back();
         }
         //添加当前元素的值和索引
-        mins.emplace_back(i,data[i]);
+        mins.emplace_back(i,data[i]);//比push_back()快;
 
         //检查队头索引范围
         while(mins.front().first <= i - k ){
