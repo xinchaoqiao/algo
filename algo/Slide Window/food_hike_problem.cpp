db10dabe4fb322944729eb362064f4d0f7923364
北之信念 <xinchaoqiao6@163.com>
2025-01-23 20:49:11 +0800

    滑行窗口问题——掘金

---
+++ algo/Slide Window/food_hike_problem.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+int solution(int n, int k, std::vector<int> data) {
+    // Edit your code here
+    deque<pair<int,int>> mins;
+    int rel = 0;
+    for(int i = 0;i < n;i++){
+        // 队列不为空 且 队尾元素大于当前元素时 移除队尾元素
+        while(!mins.empty() && mins.back().second > data[i]){
+            mins.pop_back();
+        }
+        //添加当前元素的值和索引
+        mins.emplace_back(i,data[i]);
+
+        //检查队头索引范围
+        while(mins.front().first <= i - k ){
+            mins.pop_front();
+        }
+        rel+=mins.front().second;
+    }
+    cout << rel << endl;
+    return rel;
+}
+
+int main() {
+    // Add your test cases here
+
+    std::cout << (solution(5, 2, {1, 2, 3, 3, 2}) == 9) << std::endl;
+    return 0;
+}
--- algo/Slide Window/slide the window.cpp
+++ algo/Slide Window/slide the window.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 const int N = 1e6 + 10;
 int n, k, hh = 0, tt = -1;
