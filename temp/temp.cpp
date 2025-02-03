353a8d3af68eb6f9a4c7dba42da39ca659bc0a6d
北之信念 <xinchaoqiao6@163.com>
2025-02-03 21:43:35 +0800

    2_3

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,41 +1,25 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <string>
-using namespace std;
+def solution(x_position: int, y_position: int) -> int:
+    d = abs(x_position - y_position)
+    l, r = 0, d
 
-// 实现 solution 函数，用于计算结果并格式化为保留两位小数的字符串
-std::string solution(int n, int x, const std::vector<int>& a) {
-    // 断言 n 是否等于向量 a 的长度
+    def cal(x):
+        q, r = divmod(x, 2)
+        w = q * (q + 1)
+        if r:
+            w += q + 1
+        return w
 
-    int sum_val = 0;
-    // 计算向量 a 中所有元素的和
-    for (int num : a) {
-        sum_val += num;
-    }
-    // 计算结果
-    double result = static_cast<double>(sum_val) / 4 / x;
-    cout << result << endl;
-    // 创建输出字符串流对象
-    std::ostringstream oss;
-    // 设置输出格式为固定点表示法
-    oss << std::fixed;
-    // 设置小数位数为两位
-    oss << std::setprecision(2);
-    // 将结果写入字符串流
-    oss << result;
-    // 将字符串流中的内容转换为字符串并返回
-    return oss.str();
-}
+    while l < r:
+        mid = (l + r) >> 1
+        if cal(mid) >= d:
+            r = mid
+        else:
+            l = mid + 1
+    return l
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    // 测试用例 1
-    std::cout << (solution(4, 1, {2, 3, 4, 5}) == "3.50") << std::endl;
-    // 测试用例 2
-    cout << (solution(10, 4, {7,1,7,16,3,16,6,5,4,3,5,1,2,11,17,7}) == "6.94") << endl;
-    cout << (solution(12, 4, {10,16,4,7,7,3,8,1,5,13,10,6}) == "5.62") << endl;
-    return 0;
-}
+
+if __name__ == '__main__':
+    print(solution(x_position = 12, y_position = 6) == 4)
+    print(solution(x_position = 34, y_position = 45) == 6)
+    print(solution(x_position = 50, y_position = 30) == 8)
+    print(solution(x_position = 0, y_position = 0) == 0)
